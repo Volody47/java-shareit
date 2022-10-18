2a6a6a38263b6ab6a72e3cd16f33b113229c20c1
Vladimir Sharapov <vladimir.sharapov@shoppertrak.com>
2022-10-17 23:01:19 -0500

    add findAllUsers endpoin into user controller

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,30 @@
 package ru.practicum.shareit.user;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserServiceImpl;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.List;
+
+
 @RestController
+@Slf4j
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        log.debug("Users quantity: {}", userService.findAllUsers().size());
+        return userService.findAllUsers();
+    }
 }
