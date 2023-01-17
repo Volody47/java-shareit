df6038a93a797143b201d293c16e52340c9edf92
Vladimir Sharapov <vladimir.sharapov@shoppertrak.com>
2023-01-17 09:00:25 -0600

    fix: change some value in AppConfig

--- src/main/java/ru/practicum/shareit/config/AppConfig.java
+++ src/main/java/ru/practicum/shareit/config/AppConfig.java
@@ -3,20 +3,23 @@ package ru.practicum.shareit.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Configuration
 @Slf4j
 public class AppConfig {
 
-    //protected static String INSTANCE_ID = null;
+    protected static String INSTANCE_ID = null;
 
 
 
-//    static {
-//        try {
-//            INSTANCE_ID = InetAddress.getLocalHost().getHostName();
-//        } catch (UnknownHostException e) {
-//            log.warn("Unable to get host name", e);
-//        }
-//    }
+    static {
+        try {
+            INSTANCE_ID = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            log.warn("Unable to get host name", e);
+        }
+    }
 
 }
